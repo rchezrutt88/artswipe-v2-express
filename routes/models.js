c4c599077d1e8c4f489a855df14abcc1be2926ad
RobertRuttenberg <rdruttenberg@gmail.com>
2017-02-14 18:20:57 -0500

    successfully returns model data

--- routes/models.js
+++ routes/models.js
@@ -2,9 +2,10 @@
  * Created by rdrutt on 2/13/17.
  */
 const router = require('express').Router();
+const data = require('../data/data.json');
 
 router.get('/', function (req, res) {
-    res.send("models!")
+    res.status(200).json(data.models)
 });
 
 module.exports = router;
