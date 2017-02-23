75323ad075da35375fe27a87f6b8f3b897590774
RobertRuttenberg <rdruttenberg@gmail.com>
2017-02-23 15:21:30 -0500

    ajax hits server, need to enable cors

--- public/javascripts/create_art.js
+++ public/javascripts/create_art.js
@@ -1,4 +1,26 @@
 /**
  * Created by rdrutt on 2/22/17.
  */
+$(document).ready(function () {
 
+    function handleSubmitArt (e) {
+        e.preventDefault();
+        $.ajax({
+            method: "POST",
+            url: "localhost:3000/arts/create",
+            data: $("#create-art-form").serialize(),
+            dataType: "json",
+        })
+            .done(function () {
+                console.log('it worked!');
+            })
+            .fail(function () {
+                console.log('it did not work!');
+            })
+    }
+
+
+    // Handler
+    $("#create-art-form").submit(handleSubmitArt);
+
+});
