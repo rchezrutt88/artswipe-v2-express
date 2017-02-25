148681867064df9d7db99e5712a27ed8f9c24d1f
RobertRuttenberg <rdruttenberg@gmail.com>
2017-02-25 15:15:22 -0500

    successfully load photos

--- public/javascripts/create_art.js
+++ public/javascripts/create_art.js
@@ -3,6 +3,66 @@
  */
 $(document).ready(function () {
 
+    var albumBucketName = 'artswipe';
+    var bucketRegion = 'us-east-1';
+    var IdentityPoolId = 'us-east-1:b37b4719-c6b5-4f1e-af83-65c725212309';
+    AWS.config.update({
+        region: bucketRegion,
+        credentials: new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: IdentityPoolId
+        })
+    });
+
+    var s3 = new AWS.S3({
+        apiVersion: '2006-03-01',
+        params: {Bucket: albumBucketName}
+    });
+
+    function addPhoto(albumName) {
+        var files = $("#fileupload").get()[0].files;
+        if (!files.length) {
+            return alert('Please choose a file to upload first.');
+        }
+        var file = files[0];
+        var fileName = file.name;
+        var albumPhotosKey = encodeURIComponent(albumName) + '//';
+
+        var photoKey = albumPhotosKey + fileName;
+        s3.upload({
+            Key: photoKey,
+            Body: file,
+            ACL: 'public-read'
+        }, function(err, data) {
+            if (err) {
+                return console.log(err.message);
+            }
+            console.log('Successfully uploaded photo.');
+            // viewAlbum(albumName);
+        });
+    }
+
+    // function uploadImageToAWS(e) {
+    //
+    //
+    //     e.preventDefault();
+    //     addPhoto('test');
+    //     // $.ajax({
+    //     //     method: "POST",
+    //     //     url: "http://artswipe.s3.amazonaws.com",
+    //     //     contentType: 'binary/octet-stream',
+    //     //     processData: false,
+    //     //     data: $("#fileupload").get()[0].files[0]
+    //     // })
+    //     //     .done(function (data) {
+    //     //         console.log(data);
+    //     //         console.log('file upload success?')
+    //     //     })
+    //     //     .fail(function (error) {
+    //     //         console.log(error)
+    //     //         console.log('file upload fail?')
+    //     //     });
+    // }
+
     function handleSubmitArt (e) {
         e.preventDefault();
         $.ajax({
@@ -11,6 +71,7 @@ $(document).ready(function () {
             data: $("#create-art-form").serialize(),
             dataType: "json",
         })
+            .then(addPhoto('test'))
             .done(function (data) {
                 console.log(data);
             })
